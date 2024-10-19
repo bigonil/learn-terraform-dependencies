74fffbc70efc312149d17334b0afadd947a58a12
bigonil <luca.bigoni@gmail.com>
2024-10-19 15:59:11 +0200

    Fix a bucket name and random_string length

--- main.tf
+++ main.tf
@@ -27,12 +27,18 @@ resource "aws_eip" "ip" {
   instance = aws_instance.example_a.id
 }
 
-resource "aws_s3_bucket" "example" { 
-    bucket = "example"
+resource "aws_s3_bucket" "lb-example" { 
+    bucket = "lb-terraform${random_string.bucket_suffix.result}"
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  upper   = false  # Only lowercase letters
+  special = false  # No special characters
 }
 
 resource "aws_s3_bucket_policy" "mycompliantpolicy" {
-  bucket = "example"
+  bucket = aws_s3_bucket.lb-example.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -44,8 +50,8 @@ resource "aws_s3_bucket_policy" "mycompliantpolicy" {
         Principal = "*"
         Action    = "s3:*"
         Resource = [
-          aws_s3_bucket.mycompliantbucket.arn,
-          "${aws_s3_bucket.mycompliantbucket.arn}/*",
+          aws_s3_bucket.lb-example.arn,
+          "${aws_s3_bucket.lb-example.arn}/*",
         ]
         Condition = {
           Bool = {
@@ -58,7 +64,7 @@ resource "aws_s3_bucket_policy" "mycompliantpolicy" {
 }
 
 resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.lb-example.bucket
 
   block_public_acls       = true
   block_public_policy     = true
@@ -71,12 +77,12 @@ resource "aws_instance" "example_c" {
   associate_public_ip_address = false
   instance_type = "t2.micro"
 
-  depends_on = [aws_s3_bucket.example]
+  depends_on = [aws_s3_bucket.lb-example]
 }
 
 module "example_sqs_queue" {
   source  = "terraform-aws-modules/sqs/aws"
   version = ">= 3.3.0"
 
-  depends_on = [aws_s3_bucket.example, aws_instance.example_c]
+  depends_on = [aws_s3_bucket.lb-example, aws_instance.example_c]
 }
